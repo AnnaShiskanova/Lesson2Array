12e6fda35736a609504af0d04828ac643bc100db
Anna <shiskanovaag@mail.ru>
2023-02-15 21:30:37 +1000

    My homework task10

---
+++ task10HomeWork/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая на вход принимает трехзначное число
+// и на выходе показывает вторую цифру этого числа
+//например 485 -> 8; 357 -> 5; 471 -> 7
+
+Console.WriteLine("Enter three-digit number");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x / 100 == 0 || x / 100 > 9)
+Console.WriteLine("This number is not correct, please enter three-digit number");
+else
+{
+    int z = x / 10;
+    int c = z % 10;
+    Console.WriteLine($"{c}");
+}
