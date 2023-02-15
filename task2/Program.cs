2dd454b091cce2d58ccedd260d02beb1c14b54ce
Anna <shiskanovaag@mail.ru>
2023-02-15 15:06:09 +1000

    Task2

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру из этого числа
+// например 456 -> 46, 532 -> 52
+Random number = new Random();
+int num = number.Next(100,1000);
+Console.WriteLine($"The first number is {num}");
+int a = num / 100;
+int b = num % 10;
+Console.WriteLine($"The number is {a}{b}");
